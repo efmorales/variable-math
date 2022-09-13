e618678db169675225da0aa361491c0abac23400
efmorales <efmorales@pucp.edu.pe>
2022-09-13 19:44:37 -0400

    update the quadratic.js for extra points and expertise

--- quadratic.js
+++ quadratic.js
@@ -19,3 +19,12 @@ Note: you'll need to do the formula twice. For `x1`, use a + in place of the ± 
 let a = 1
 let b = 9
 let c = 2
+
+let b2 = b - 18 // -9
+let op1 = Math.sqrt((b ** 2) - (4 * a * c)) // 8.544
+
+let x1 = (b2 + op1) / 2 * a
+let x2 = (b2 - op1) / 2 * a
+
+console.log(`For the fist version of the cuadratic formula, the addition ver. has the result of: ${x1}`)
+console.log(`For the fist version of the cuadratic formula, the substraction ver. has the result of: ${x2}`)
